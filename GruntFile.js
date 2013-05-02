f47b57cca92a255228397e99339bd26bc2c0c6d1
Jesse Freeman <jessefreeman@gmail.com>
2013-05-01 22:32:24 -0400

    initial import

---
+++ GruntFile.js
@@ -0,0 +1,41 @@
+module.exports = function (grunt) {
+
+    grunt.loadNpmTasks('grunt-typescript');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-open');
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        connect: {
+            server: {
+                options: {
+                    port: 8080,
+                    base: './deploy'
+                }
+            }
+        },
+        typescript: {
+            base: {
+                src: ['src/**/*.ts'],
+                dest: 'deploy/js/game.js',
+                options: {
+                    module: 'amd',
+                    target: 'es5'
+                }
+            }
+        },
+        watch: {
+            files: '**/*.ts',
+            tasks: ['typescript']
+        },
+        open: {
+            dev: {
+                path: 'http://localhost:8080/index.html'
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['connect', 'open', 'watch']);
+
+}
